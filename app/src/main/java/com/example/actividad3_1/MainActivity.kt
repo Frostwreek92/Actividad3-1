00f8b9857fa33a8584b1fcde45402a0d835ff57c
Frostwreek92 <pamorellut666@gmail.com>
2025-12-05 16:40:27 +0100

    Finish

--- app/src/main/java/com/example/actividad3_1/MainActivity.kt
+++ app/src/main/java/com/example/actividad3_1/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.actividad3_1
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -11,6 +12,9 @@ import androidx.compose.ui.Modifier
 import com.example.actividad3_1.codes.DessertClickerApp
 import com.example.actividad3_1.data.Datasource
 import com.example.actividad3_1.ui.theme.Actividad31Theme
+
+
+private const val TAG = "MainActivity"
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,5 +32,29 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart Called")
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume Called")
+    }
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart Called")
+    }
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause Called")
+    }
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop Called")
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy Called")
     }
 }
